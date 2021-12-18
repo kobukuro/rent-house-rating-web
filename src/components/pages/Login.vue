c6f27e621f526049ea6b8f0d827e9204194a0921
PeterChen <sanpeter992000@gmail.com>
2021-12-18 22:23:34 +0800

    add Login page

---
+++ src/components/pages/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app id="inspire">
+    <v-main>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Login form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                      v-model="form.email"
+                      prepend-icon="person"
+                      name="login"
+                      label="Email"
+                      type="text"
+                  ></v-text-field>
+                  <v-text-field
+                      id="password"
+                      v-model="form.password"
+                      prepend-icon="lock"
+                      name="password"
+                      label="Password"
+                      type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="login">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    login() {
+
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import "vuetify/dist/vuetify.min.css"; // Add this line
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-});
+export default new Vuetify({});
