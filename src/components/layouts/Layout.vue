c580d796519e8ead16bc7ed7d64f5e2ed854cb84
PeterChen <sanpeter992000@gmail.com>
2021-12-21 22:04:29 +0800

    make v-menu not be blocked

--- src/components/layouts/Layout.vue
+++ src/components/layouts/Layout.vue
@@ -1,36 +1,64 @@
 <template>
   <div class="layout-default">
-    <el-header height="auto">
-      <el-row>
-        <div class="right-menu">
-          <el-tooltip content="Language" effect="dark" placement="bottom">
-            <el-dropdown trigger="click" @command="handleLanguageCommand">
-              <el-button id="btn_lang_toggle" size="medium" circle>
-                <v-icon-awesome name="globe-asia"></v-icon-awesome>
-              </el-button>
-              <el-dropdown-menu>
-                <el-dropdown-item id="option_english" command="en_us">English</el-dropdown-item>
-                <el-dropdown-item id="option_traditional_chinese" command="zh_tw">繁體中文</el-dropdown-item>
-                <el-dropdown-item id="option_simplified_chinese" command="zh_cn">日本語</el-dropdown-item>
-              </el-dropdown-menu>
-            </el-dropdown>
-          </el-tooltip>
-          <el-dropdown @command="handleCommand">
-            <el-button type="medium">
-              {{ username }}<i class="el-icon-arrow-down el-icon--right"></i>
-            </el-button>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item command="logout">
-                <v-icon-awesome name="sign-out-alt"></v-icon-awesome>
-                Logout
-              </el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
-        </div>
-      </el-row>
-    </el-header>
+    <!--    <el-header height="auto">-->
+    <!--      <el-row>-->
+    <!--        <div class="right-menu">-->
+    <!--          <el-tooltip content="Language" effect="dark" placement="bottom">-->
+    <!--            <el-dropdown trigger="click" @command="handleLanguageCommand">-->
+    <!--              <el-button id="btn_lang_toggle" size="medium" circle>-->
+    <!--                <v-icon-awesome name="globe-asia"></v-icon-awesome>-->
+    <!--              </el-button>-->
+    <!--              <el-dropdown-menu>-->
+    <!--                <el-dropdown-item id="option_english" command="en_us">English</el-dropdown-item>-->
+    <!--                <el-dropdown-item id="option_traditional_chinese" command="zh_tw">繁體中文</el-dropdown-item>-->
+    <!--                <el-dropdown-item id="option_simplified_chinese" command="zh_cn">日本語</el-dropdown-item>-->
+    <!--              </el-dropdown-menu>-->
+    <!--            </el-dropdown>-->
+    <!--          </el-tooltip>-->
+    <!--          <el-dropdown @command="handleCommand">-->
+    <!--            <el-button type="medium">-->
+    <!--              {{ username }}<i class="el-icon-arrow-down el-icon&#45;&#45;right"></i>-->
+    <!--            </el-button>-->
+    <!--            <el-dropdown-menu slot="dropdown">-->
+    <!--              <el-dropdown-item command="logout">-->
+    <!--                <v-icon-awesome name="sign-out-alt"></v-icon-awesome>-->
+    <!--                Logout-->
+    <!--              </el-dropdown-item>-->
+    <!--            </el-dropdown-menu>-->
+    <!--          </el-dropdown>-->
+    <!--        </div>-->
+    <!--      </el-row>-->
+    <!--    </el-header>-->
+    <v-toolbar>
+      <v-spacer></v-spacer>
+      <v-menu offset-y style="position: relative; z-index: 3">
+        <template v-slot:activator="{ on }">
+          <v-btn
+              dark
+              icon
+              v-on="on"
+          >
+            <v-icon color="primary">notifications</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-list dense>
+            <v-subheader>Notifications</v-subheader>
+            <v-divider></v-divider>
+            <v-list-tile
+                v-for="notification in notifications"
+                :key="`notification-key-${notification.id}`"
+            >
+              <v-list-tile-title>
+                {{ notification.title }}
+              </v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-menu>
+    </v-toolbar>
     <el-main>
-      <main-page/>
+      <main-page style="position: relative; z-index: 3"/>
     </el-main>
   </div>
 </template>
@@ -42,6 +70,16 @@ import MainPage from "@/components/pages/MainPage";
 export default {
   name: "Layout",
   components: {MainPage},
+  data() {
+    return {
+      notifications: [
+        {id: 1, title: 'Click Me'},
+        {id: 2, title: 'Click Me'},
+        {id: 3, title: 'Click Me'},
+        {id: 4, title: 'Click Me 2'}
+      ]
+    }
+  },
   methods: {
     handleCommand(command) {
       switch (command) {
