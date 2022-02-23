15a437572355e9961032abe1cd3cd4d8f4e0561d
PeterChen <sanpeter992000@gmail.com>
2022-02-23 21:24:34 +0800

    add "add location" dialog

--- src/components/pages/MainPage.vue
+++ src/components/pages/MainPage.vue
@@ -41,14 +41,57 @@
       <l-layer-group ref="newLayerGroup">
         <l-popup>
           <div>
-            <v-btn
-                small
-                icon
-                @click="showLocationModalMethod">
-              <v-icon dark>
-                mdi-map-marker-plus
-              </v-icon>
-            </v-btn>
+            <v-dialog
+                v-model="dialog"
+                width="500"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                    small
+                    icon
+                    v-bind="attrs"
+                    v-on="on"
+                >
+                  <!--@click="showLocationModalMethod"-->
+                  <v-icon dark>
+                    mdi-map-marker-plus
+                  </v-icon>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title class="text-h5 grey lighten-2">
+                  Add Location
+                </v-card-title>
+
+                <v-text-field
+                    v-model="locationToAdd.address"
+                    label="address"
+                    required
+                ></v-text-field>
+                <v-text-field
+                    v-model="locationToAdd.ownerName"
+                    label="ownerName"
+                    required
+                ></v-text-field>
+                <!--                <v-divider></v-divider>-->
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                      color="#BDBDBD"
+                      @click="dialog = false"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                      color="primary"
+                      @click="addLocation"
+                  >
+                    Submit
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
           </div>
         </l-popup>
       </l-layer-group>
@@ -107,6 +150,7 @@ export default {
   // components: {ContextMenu},
   data() {
     return {
+      dialog: false,
       locationToAdd: {
         country_name: '',
         country_id: 0,
@@ -200,7 +244,8 @@ export default {
           })
     },
     showLocationModalMethod() {
-      this.$refs['addLocationModal'].show()
+      //要加上自動抓取的地址
+      // this.$refs['addLocationModal'].show()
     },
     showRatingModalMethod() {
       this.$refs['addRatingModal'].show()
@@ -222,6 +267,7 @@ export default {
               ratings: []
             }
             this.data.push(element)
+            this.dialog = false
           })
     },
     addRating() {
